4706f599449b3cd8f046303451fddfdeed8046e9
MarahShabello <s11819990@stu.najah.edu>
2023-03-25 23:32:29 +0300

    Edit header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import BoxSize from 'react-box-size';
 import { styled } from '@mui/material/styles';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import { AppBar, Box, Toolbar, Button, Typography } from '@mui/material';
+
+const StyledAppBar = styled(AppBar)(() => ({
+  backgroundColor: '#fff',
+  width: '100%',
+  position: 'static'
+}));
 
 const StyledTypography = styled(Typography)(() => ({
   color: '#111517',
@@ -12,28 +19,34 @@ const StyledTypography = styled(Typography)(() => ({
 const StyledButton = styled(Button)(() => ({
   color: '#111517',
   fontFamily: 'Nunito Sans',
-  fontWeight: '600',
+  fontWeight: '800',
   fontSize: '13px'
 }));
 
 const StyledToolbar = styled(Toolbar)(() => ({
-  backgroundColor: '#fff'
+  backgroundColor: 'inherit'
+}));
+
+const StyledDarkModeOutlinedIcon = styled(DarkModeOutlinedIcon)(() => ({
+  marginInlineEnd: '5px'
 }));
 
 function AppHeader() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{width: '100%'}}>
-        <StyledToolbar>
-          <StyledTypography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Where in the world?
-          </StyledTypography>
-          <StyledButton color="inherit">
-            <DarkModeOutlinedIcon />
-            Dark Mode
-          </StyledButton>
-        </StyledToolbar>
-      </AppBar>
+      <StyledAppBar>
+        <BoxSize pv={1} ph={4}>
+          <StyledToolbar>
+            <StyledTypography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Where in the world?
+            </StyledTypography>
+            <StyledButton color="inherit">
+              <StyledDarkModeOutlinedIcon />
+              Dark Mode
+            </StyledButton>
+          </StyledToolbar>
+        </BoxSize>
+      </StyledAppBar>
     </Box>
   );
 }
